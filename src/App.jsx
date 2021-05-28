75d9058d4a7a7cd037ee62bce6c338442f5c1f95
NilsonKr <nuotrok444@gmail.com>
2021-05-28 03:34:05 -0500

    :tada: [INITIAL] Structure Base

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className='container'>
+			<h1>Hello WhatsDown</h1>
+		</div>
+	);
+};
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/**
+  @type {import('webpack').Configuration} 
+*/
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.js',
+		publicPath: '/',
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.(css|s[ac]ss)$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /.html$/,
+				use: 'html-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		historyApiFallback: true,
+	},
+};
