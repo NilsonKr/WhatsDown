867010728e301aabcc0e0e10ae05ccef1fe63880
NilsonKr <nuotrok444@gmail.com>
2021-05-31 03:39:33 -0500

    :sparkles: [ADD] Set Emoji Status

--- src/containers/ProfileContainer.jsx
+++ src/containers/ProfileContainer.jsx
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Picker } from 'emoji-mart';
 
+import 'emoji-mart/css/emoji-mart.css';
 import ProfileIcon from '../../assets/user.svg';
+
+const EmojiPickerStyles = {
+	position: 'absolute',
+	bottom: '0',
+	left: '0',
+	transform: 'translateY(100%)',
+	width: '250px',
+	height: '250px',
+	overflow: 'hidden',
+};
 
 const ProfileContainer = () => {
 	const [emoji, setEmoji] = useState(' ');
@@ -15,6 +27,11 @@ const ProfileContainer = () => {
 				<div className='profile__img'>
 					<img src={ProfileIcon} alt='User' />
 					<span>{emoji}</span>
+					<Picker
+						set='apple'
+						onSelect={emoji => setEmoji(emoji.native)}
+						style={EmojiPickerStyles}
+					/>
 				</div>
 				<div className='profile__text'>
 					<h2>Nilson Diaz</h2>
