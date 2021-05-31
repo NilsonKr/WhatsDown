d83902fcd57900271aa8b5ee379c7079dd59a061
NilsonKr <nuotrok444@gmail.com>
2021-05-31 02:09:55 -0500

    :lipstick: [ADD] Profile Status Emoji

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -6,7 +6,10 @@ const Messages = () => {
 	return (
 		<section className='messages__container'>
 			<div className='messages__chat'>
-				<img src={userImg} alt='' />
+				<div className='messages__profile'>
+					<img src={userImg} alt='' />
+					<span className='messages--status'>🦄</span>
+				</div>
 				<div className='messages__chat--info'>
 					<h2>Nilson Diaz</h2>
 					<p>Hello world Whatsdown!!..</p>
@@ -14,7 +17,10 @@ const Messages = () => {
 				<p className='messages__chat--date'>08:05</p>
 			</div>
 			<div className='messages__chat not-seen'>
-				<img src={userImg} alt='' />
+				<div className='messages__profile'>
+					<img src={userImg} alt='' />
+					<span className='messages--status'>🐧</span>
+				</div>
 				<div className='messages__chat--info'>
 					<h2>
 						Minari <span className='chat--notifaction'></span>
