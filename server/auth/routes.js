2de6a1ade4a9ef87fd2310f4d757a4e681438a80
NilsonKr <nuotrok444@gmail.com>
2021-06-23 03:03:44 -0500

    :sparkles: [ADD] Login endpoint Done
    Setting JWT in a cookie and response with user info

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const axios = require('axios').default;
 const boom = require('@hapi/boom');
+const config = require('../config/index');
 
 //Basic Strategy
 require('./strategies/basicStrategy');
@@ -10,10 +11,37 @@ function authRoutes(app) {
 	const router = express.Router();
 	app.use('/auth/', router);
 
-	router.post('/login', passport.authenticate('basic', { session: false }), (req, res, next) => {
-		console.log(req.user);
+	router.post('/login', passport.authenticate('basic', { session: false }), async (req, res, next) => {
+		if (!req.user) {
+			return next(boom.unauthorized());
+		}
 
-		res.end();
+		const { token, user } = req.user;
+
+		//Get Authorizing JWT
+		try {
+			const { data, status } = await axios({
+				method: 'post',
+				url: `${config.apiUrl}/auth/authorizate`,
+				headers: { Authorization: `Bearer ${token}` },
+			});
+
+			if (status !== 200) {
+				return next(boom.unauthorized());
+			}
+
+			//Set token Cookie
+
+			res.cookie('token', data.token, {
+				maxAge: '14400000',
+			});
+
+			res.status(200).json({
+				user,
+			});
+		} catch (error) {
+			next(error);
+		}
 	});
 }
 
