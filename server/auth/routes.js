0a0fd6893e84e975868af7a5262b9af64889ed8c
NilsonKr <nuotrok444@gmail.com>
2021-06-24 02:29:11 -0500

    :sparkles: [ADD] Singup Enpoint and Setting expire times for cookies

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -4,6 +4,8 @@ const axios = require('axios').default;
 const boom = require('@hapi/boom');
 const config = require('../config/index');
 
+const FOUR_HOURS = '14400000';
+
 //Basic Strategy
 require('./strategies/basicStrategy');
 
@@ -17,6 +19,7 @@ function authRoutes(app) {
 		}
 
 		const { token, user } = req.user;
+		const { remember } = req.query;
 
 		//Get Authorizing JWT
 		try {
@@ -31,13 +34,38 @@ function authRoutes(app) {
 			}
 
 			//Set token Cookie
+			const time = remember === 'true' ? config.rememberTime : FOUR_HOURS;
 
 			res.cookie('token', data.token, {
-				maxAge: '14400000',
+				httpOnly: config.env === 'development' ? false : true,
+				secure: config.env === 'development' ? false : true,
+				maxAge: time,
 			});
 
 			res.status(200).json({
 				user,
+			});
+		} catch (error) {
+			next(error);
+		}
+	});
+
+	router.post('/signup', async (req, res, next) => {
+		try {
+			const { data, status } = await axios({
+				method: 'post',
+				url: `${config.apiUrl}/auth/sign-up`,
+				data: {
+					user: req.body,
+				},
+			});
+
+			if (status !== 201) {
+				return next(boom.badRequest());
+			}
+
+			res.status(201).json({
+				user: data,
 			});
 		} catch (error) {
 			next(error);
--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const config = {
 	env: process.env.NODE_ENV || 'development',
+	rememberTime: 1000 * 60 * 60 * 24 * 30,
 	port: process.env.PORT || 8000,
 	apiUrl: process.env.API_URL,
 	apiToken: process.env.API_TOKEN,
