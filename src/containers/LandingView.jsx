1eb7983a6dfd9b23a335ccffbc4427dbdad4f84a
NilsonKr <nuotrok444@gmail.com>
2021-07-18 15:22:39 -0500

    :lipstick: [ADD] Responsive Styles for Landing & login/Signup Sections

--- src/containers/LandingView.jsx
+++ src/containers/LandingView.jsx
@@ -6,22 +6,24 @@ const LandingView = () => {
 		<section className='landing__container'>
 			<div className='bg__el bg--left'></div>
 			<div className='bg__el bg--right'></div>
-			<div className='chat__view'>
-				<img src='assets/LandingChat.jpg' alt='Chat Preview' />
+			<div className='landing__content'>
+				<div className='chat__view'>
+					<img src='assets/LandingChat.jpg' alt='Chat Preview' />
+				</div>
+				<div className='landing__info'>
+					<h2 className='landing--tittle'>WhatsDown</h2>
+					<p className='landing--text'>
+						Stay touch with your acquaintances, will make your life easier, regarding
+						organizing your social life, What are you waiting for Join Us!?
+					</p>
+				</div>
+				<Link to='/signUp' className='landing--singup'>
+					Sign Up
+				</Link>
+				<Link to='/logIn' className='landing--login'>
+					Have an Account Already? Sign in
+				</Link>
 			</div>
-			<div className='landing__info'>
-				<h2 className='landing--tittle'>WhatsDown</h2>
-				<p className='landing--text'>
-					Stay touch with your acquaintances, will make your life easier, regarding organizing your social
-					life, What are you waiting for Join Us!?
-				</p>
-			</div>
-			<Link to='/signUp' className='landing--singup'>
-				Sign Up
-			</Link>
-			<Link to='/logIn' className='landing--login'>
-				Have an Account Already? Sign in
-			</Link>
 		</section>
 	);
 };
